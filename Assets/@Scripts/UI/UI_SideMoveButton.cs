4b3b283d95ff11275a376c5dfbf04184872224e2
Hajun Song <songhajun648@gmail.com>
2025-01-22 16:24:13 +0900

    create UI_SideMoveButton Prefab

---
+++ Assets/@Scripts/UI/UI_SideMoveButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_SideMoveButton : UI_Base
+{
+    enum GameObjects
+    {
+        JoystickBG,
+        JoystickCursor,
+    }
+}
